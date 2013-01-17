94ded8c2325c2f4ae2fcc47dba97ae8d66e939f6
Geoff Thornburrow <geofftnz@gmail.com>
2013-01-17 21:51:52 +1300

    Render a square.

--- snowscape/TerrainGenerationViewer/TerrainGenerationViewer.cs
+++ snowscape/TerrainGenerationViewer/TerrainGenerationViewer.cs
@@ -36,23 +36,66 @@ namespace Snowscape.TerrainGenerationViewer
 
             // GL state
             GL.Enable(EnableCap.DepthTest);
-            GL.ClearColor(new Color4(0, 24, 64, 255));
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-1.0, 1.0, 1.0, -1.0, 0.001, 10.0);
+            GL.MatrixMode(MatrixMode.Modelview);
 
             base.OnLoad(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(this.ClientRectangle);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-1.0, 1.0, 1.0, -1.0, 0.001, 10.0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            base.OnResize(e);
         }
 
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
-            base.OnUpdateFrame(e);
+            //base.OnUpdateFrame(e);
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
+            GL.ClearColor(new Color4(0, 96, 64, 255));
+            GL.ClearDepth(10.0);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.PushMatrix();
+            GL.LoadIdentity();
+            GL.Translate(0.0, 0.0, -1.0);
+            GL.Scale(0.5, 0.5, 0.5);
+
+            //GL.Disable(EnableCap.Lighting);
+            //GL.Disable(EnableCap.Texture2D);
+            //GL.CullFace(CullFaceMode.Back);
+            //GL.LineWidth(2.0f);
+
+            //GL.Enable(EnableCap.Blend);
+            //GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+
+            GL.Begin(BeginMode.TriangleStrip);
+
+            GL.Color4(1.0f,0.5f,0.0f,0.5f);
+
+            GL.Vertex3(-1.0f, -1.0f, 0.0f);
+            GL.Vertex3(-1.0f, 1.0f, 0.0f);
+            GL.Vertex3(1.0f, -1.0f, 0.0f);
+            GL.Vertex3(1.0f, 1.0f, 0.0f);
+
+            GL.End();
+
+            GL.PopMatrix();
+
             SwapBuffers();
-            base.OnRenderFrame(e);
+            //base.OnRenderFrame(e);
         }
 
     }
