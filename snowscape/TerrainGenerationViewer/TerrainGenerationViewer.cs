e9076796c5051c054311e42f691ca9264d7a419c
Geoff Thornburrow <geofftnz@gmail.com>
2013-02-10 00:16:57 +1300

    contour lines, smooth heights and normals

--- snowscape/TerrainGenerationViewer/TerrainGenerationViewer.cs
+++ snowscape/TerrainGenerationViewer/TerrainGenerationViewer.cs
@@ -108,15 +108,41 @@ in vec2 texcoord0;
 out vec4 out_Colour;
 
 const float TEXEL = 1.0 / 1024.0;
+const float NTEXEL = 0.5 / 1024.0;
+const float SMTEXEL = 0.25 / 1024.0;
+
+float sampleHeight(vec2 p)
+{
+    vec2 p0 = trunc(p * 1024.0) /1024.0;
+    vec2 pf = clamp((p-p0) * 1024.0,0.0,1.0);
+
+    float h00 = texture2D(heightTex,vec2(p0.x,p0.y)).r;
+    float h01 = texture2D(heightTex,vec2(p0.x,p0.y+TEXEL)).r;
+    float h10 = texture2D(heightTex,vec2(p0.x+TEXEL,p0.y)).r;
+    float h11 = texture2D(heightTex,vec2(p0.x+TEXEL,p0.y+TEXEL)).r;
+
+    return mix(mix(h00,h10,pf.x),mix(h01,h11,pf.x),pf.y);
+}
 
 vec3 getNormal(vec2 pos)
 {
-    float h1 = texture2D(heightTex,vec2(pos.x,pos.y-TEXEL)).r;
-    float h2 = texture2D(heightTex,vec2(pos.x,pos.y+TEXEL)).r;
-    float h3 = texture2D(heightTex,vec2(pos.x-TEXEL,pos.y)).r;
-    float h4 = texture2D(heightTex,vec2(pos.x+TEXEL,pos.y)).r;
+    float h1 = sampleHeight(vec2(pos.x,pos.y-NTEXEL)).r;
+    float h2 = sampleHeight(vec2(pos.x,pos.y+NTEXEL)).r;
+    float h3 = sampleHeight(vec2(pos.x-NTEXEL,pos.y)).r;
+    float h4 = sampleHeight(vec2(pos.x+NTEXEL,pos.y)).r;
 
-    return normalize(vec3(h4-h3,h2-h1,2.0*TEXEL));
+    return normalize(vec3(h4-h3,h2-h1,2.0*NTEXEL));
+}
+
+float contour(float h0, float h1,float h2, float h3, float h4, float contourscale)
+{
+	float b0 = floor(h0*contourscale);
+	float b1 = floor(h1*contourscale);
+	float b2 = floor(h2*contourscale);
+	float b3 = floor(h3*contourscale);
+	float b4 = floor(h4*contourscale);
+
+	return (b0>b1 || b0>b2 || b0>b3 || b0>b4) ? 1.0:0.0;
 }
 
 
@@ -133,7 +159,7 @@ void main(void)
 	vec4 colE = vec4(1.0,0.4,0.0,1.0);
 
 	vec4 s = texture2D(shadeTex,texcoord0.st);
-	float h = texture2D(heightTex,texcoord0.st).r;
+	float h = sampleHeight(texcoord0.st);
 
 	float looseblend = clamp(s.r * s.r * 8.0,0.0,1.0);
 	vec4 col = mix(mix(colH1,colH2,h),mix(colL1,colL2,h),looseblend);
@@ -154,6 +180,17 @@ void main(void)
 
 	float diffuse = clamp(dot(n,l) * 0.5 + 0.5,0,1);
 	col *= (0.4 + 0.6 * diffuse);
+
+	float h1 = sampleHeight(texcoord0.st + vec2(0,-SMTEXEL));
+	float h2 = sampleHeight(texcoord0.st + vec2(0,SMTEXEL));
+	float h3 = sampleHeight(texcoord0.st + vec2(-SMTEXEL,0));
+	float h4 = sampleHeight(texcoord0.st + vec2(SMTEXEL,0));
+
+    vec4 contourCol = vec4(1.0,0.5,0.0,1.0);
+
+	col += contourCol * contour(h,h1,h2,h3,h4,500.0) * 0.05;
+	col += contourCol * contour(h,h1,h2,h3,h4,50.0) * 0.1;
+
 
     out_Colour = vec4(col.rgb,1.0);
   
