d42aa9a8c8ace9fa524da791acd5a0d7d2fb96f8
Geoff Thornburrow <geofftnz@gmail.com>
2013-03-14 15:12:51 +1300

    upgrading OpenTK

---
+++ snowscape/TerrainRenderer/TerrainTile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace Snowscape.TerrainRenderer
+{
+
+    /// <summary>
+    /// A Terrain Tile...
+    /// 
+    /// knows about:
+    /// - its bounding box (VBO)
+    /// - its heightmap (texture)
+    /// - its normalmap (texture)
+    /// - its shading data (textures)
+    /// 
+    /// knows how to:
+    /// - generate its bounding box from the supplied (full-scale) heightmap
+    /// - generate its textures from a subset of the supplied full-scale textures
+    /// </summary>
+    public class TerrainTile
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+    }
+}
