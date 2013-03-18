895206dcdeceeced4f203d77672543ef92adb1c3
Geoff Thornburrow <geofftnz@gmail.com>
2013-03-18 21:21:15 +1300

    skeleton renderers

---
+++ snowscape/TerrainRenderer/Renderers/RaycastRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace Snowscape.TerrainRenderer.Renderers
+{
+    public class RaycastRenderer : ITileRenderer
+    {
+        public void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Render(TerrainTile tile, Matrix4 projection, Matrix4 view, Vector3 eye)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unload()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ snowscape/TerrainRenderer/Renderers/RendererHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace Snowscape.TerrainRenderer.Renderers
+{
+    internal static class RendererHelper
+    {
+
+        public static Vector4 GetBoxParam(this TerrainTile tile)
+        {
+            return new Vector4((float)tile.Width, (float)tile.Height, tile.MinHeight, tile.MaxHeight);
+        }
+
+    }
+}
