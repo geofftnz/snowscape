da9db43cef5e47b801ec59044a8bbbd7ef4ca7ad
Geoff Thornburrow <geofftnz@gmail.com>
2013-03-22 14:07:12 +1300

    start of terrain tile border

---
+++ snowscape/Resources/Shaders/TerrainTileMeshBorder.frag
@@ -0,0 +1,32 @@
+﻿#version 140
+precision highp float;
+
+uniform vec4 boxparam;
+
+in vec3 boxcoord;
+in vec3 worldpos;
+in vec4 normal;
+in vec4 shade;
+in vec4 param;
+
+out vec4 out_Pos;
+out vec4 out_Normal;
+out vec4 out_Shade;
+out vec4 out_Param;
+
+void main(void)
+{
+    // generate world coordinate from offset, relative to eye
+	//vec3 worldPos = (boxcoord + tileOffset) - eyePos;
+    
+	vec2 texcoord = boxcoord.xz/boxparam.xy;
+    float h = texture2D(heightTex,texcoord).r;
+    vec3 normal = normalize(texture2D(normalTex,texcoord).rgb - vec3(0.5,0.5,0.5));
+    vec4 shade = texture2D(shadeTex,texcoord);
+
+    out_Pos = vec4(worldpos.xyz,1.0);
+    out_Normal = vec4(normal.xyz * 0.5 + 0.5,1.0);
+    out_Shade = vec4(shade.xyz,1.0);
+    out_Param = vec4(h / 255.0,h / 512.0-0.3,0.4-h / 255.0,1.0);
+    //out_Colour = vec4(boxcoord.xyz / 255.0,1.0);
+}
---
+++ snowscape/TerrainRenderer/TileMeshBorder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTKExtensions;
+using Utils;
+
+namespace Snowscape.TerrainRenderer
+{
+
+    /// <summary>
+    /// The one-cell border required to fill the seam on 2 edges of a mesh tile.
+    /// 
+    /// knows about:
+    /// - the right/bottom border of the current tile
+    /// - the left border of the tile to the right
+    /// - the top border of the tile below
+    /// - the top-left point of the tile to the bottom-right
+    /// </summary>
+    public class TileMeshBorder
+    {
+        public TerrainTile Tile { get; set; }
+
+        private VBO vertexVBO = new VBO("bordervertex");
+        private VBO boxcoordVBO = new VBO("borderboxcoord");
+
+        // stuff normally specified via textures
+        private VBO heightVBO = new VBO("borderheight");
+        private VBO normalVBO = new VBO("bordernormal");
+        private VBO shadeVBO = new VBO("bordershade");
+        private VBO paramVBO = new VBO("borderparam");
+
+        private VBO indexVBO = new VBO("borderindex", BufferTarget.ElementArrayBuffer);
+        private ShaderProgram boundingBoxProgram = new ShaderProgram("tilemesh");
+
+
+        public TileMeshBorder(TerrainTile tile)
+        {
+            this.Tile = tile;
+        }
+
+        public void Init()
+        {
+            InitShader();
+        }
+
+        public void SetData()
+        {
+
+        }
+
+
+        private void InitShader()
+        {
+            // setup shader
+            this.boundingBoxProgram.Init(
+                @"../../../Resources/Shaders/TerrainTileMeshBorder.vert".Load(),
+                @"../../../Resources/Shaders/TerrainTileMeshBorder.frag".Load(),
+                new List<Variable> 
+                { 
+                    new Variable(0, "vertex"), 
+                    new Variable(1, "in_boxcoord"),
+                    new Variable(2, "in_normal"),
+                    new Variable(3, "in_shade"),
+                    new Variable(4, "in_param"),
+                },
+                new string[]
+                {
+                    "out_Pos",
+                    "out_Normal",
+                    "out_Shade",
+                    "out_Param"
+                });
+        }
+
+    }
+}
