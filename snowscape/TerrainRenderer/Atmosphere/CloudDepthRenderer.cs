915a6a6da3919edaed995f3ce36fc761912a6f6e
Geoff Thornburrow <geofftnz@gmail.com>
2013-05-13 21:58:56 +1200

    convert cloud depth to gbuffer step

--- snowscape/TerrainRenderer/Atmosphere/CloudDepthRenderer.cs
+++ snowscape/TerrainRenderer/Atmosphere/CloudDepthRenderer.cs
@@ -27,17 +27,9 @@ namespace Snowscape.TerrainRenderer.Atmosphere
     /// </summary>
     public class CloudDepthRenderer
     {
-         // Needs:
-        // Quad vertex VBO
-        private VBO vertexVBO = new VBO("cloud-depth-vertex");
-        // Quad index VBO
-        private VBO indexVBO = new VBO("cloud-depth-index", BufferTarget.ElementArrayBuffer);
+        // Needs:
+        private GBufferShaderStep gb = new GBufferShaderStep("cloud_depth");
 
-        // GBuffer to encapsulate our output texture.
-        private GBuffer gbuffer = new GBuffer("cloud-depth-gbuffer", false);
-
-        // shader
-        private ShaderProgram program = new ShaderProgram("cloud-depth");
 
         public CloudDepthRenderer()
         {
@@ -45,78 +37,23 @@ namespace Snowscape.TerrainRenderer.Atmosphere
 
         public void Init(Texture outputTexture)
         {
-            // init VBOs
-            this.InitVBOs();
+            gb.SetOutputTexture(0, "out_CloudDepth", outputTexture);
+            gb.Init(@"../../../Resources/Shaders/CloudDepth.vert".Load(), @"../../../Resources/Shaders/CloudDepth.frag".Load());
 
-            // init GBuffer
-            this.InitGBuffer(outputTexture);
-
-            // init Shader
-            this.InitShader();
-
-        }
-
-        private void InitShader()
-        {
-            // setup shader
-            this.program.Init(
-                @"../../../Resources/Shaders/CloudDepth.vert".Load(),
-                @"../../../Resources/Shaders/CloudDepth.frag".Load(),
-                new List<Variable> 
-                { 
-                    new Variable(0, "vertex")
-                },
-                new string[]
-                {
-                    "out_CloudDepth"
-                });            
-        }
-
-        private void InitGBuffer(Texture outputTexture)
-        {
-            gbuffer.SetSlot(0, outputTexture);
-            gbuffer.Init(outputTexture.Width, outputTexture.Height);
-        }
-
-        private void InitVBOs()
-        {
-            Vector3[] vertex = {
-                                    new Vector3(-1f,1f,0f),
-                                    new Vector3(-1f,-1f,0f),
-                                    new Vector3(1f,1f,0f),
-                                    new Vector3(1f,-1f,0f)
-                                };
-            uint[] index = {
-                                0,1,2,
-                                1,3,2
-                            };
-
-            this.vertexVBO.SetData(vertex);
-            this.indexVBO.SetData(index);
         }
 
         public void Render(Texture cloudTexture, Vector3 sunVector, Vector3 cloudScale)
         {
-
-            // start gbuffer
-            this.gbuffer.BindForWriting();
-            
-            GL.Disable(EnableCap.Blend);
-            GL.Disable(EnableCap.DepthTest);
-
-            cloudTexture.Bind(TextureUnit.Texture0);
-
-            this.program
-                .UseProgram()
-                .SetUniform("cloudTexture", 0)
-                .SetUniform("sunDirection", sunVector)
-                .SetUniform("cloudScale", cloudScale);
-            this.vertexVBO.Bind(this.program.VariableLocation("vertex"));
-            this.indexVBO.Bind();
-            GL.DrawElements(BeginMode.Triangles, this.indexVBO.Length, DrawElementsType.UnsignedInt, 0);
-
-            this.gbuffer.UnbindFromWriting();
-
+            gb.Render(() =>
+            {
+                cloudTexture.Bind(TextureUnit.Texture0);
+            },
+            (sp) =>
+            {
+                sp.SetUniform("cloudTexture", 0);
+                sp.SetUniform("sunDirection", sunVector);
+                sp.SetUniform("cloudScale", cloudScale);
+            });
         }
     }
 }
