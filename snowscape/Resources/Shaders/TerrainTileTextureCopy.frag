b93c1c0d01402c1e73ab05825f71c3db30eb4c3e
Geoff Thornburrow <geofftnz@gmail.com>
2014-02-28 21:34:52 +1300

    added texture tile copy

---
+++ snowscape/Resources/Shaders/TerrainTileTextureCopy.frag
@@ -0,0 +1,15 @@
+﻿#version 140
+precision highp float;
+
+uniform sampler2D heightTex;
+uniform sampler2D paramTex;
+in vec2 texcoord;
+
+out float out_Height;
+out vec4 out_Param;
+
+void main(void)
+{
+	out_Height = texture2D(heightTex,texcoord).r;
+	out_Param = texture2D(paramTex,texcoord);
+}
---
+++ snowscape/TerrainRenderer/Utility/TerrainTextureCopy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTKExtensions;
+using OpenTK.Graphics.OpenGL;
+using OpenTK;
+using Utils;
+
+namespace Snowscape.TerrainRenderer.Utility
+{
+    /// <summary>
+    /// Copies height and param textures from GPU terrain generation to terrain tile via a FBO.
+    /// 
+    /// </summary>
+    public class TerrainTileTextureCopy
+    {
+        private GBufferShaderStep gb = new GBufferShaderStep("terrain tex copy");
+
+        public TerrainTileTextureCopy()
+        {
+
+        }
+
+        public void Init(Texture heightTexture, Texture paramTexture)
+        {
+            gb.SetOutputTexture(0, "out_Height", heightTexture);
+            gb.SetOutputTexture(1, "out_Param", paramTexture);
+            gb.Init(@"../../../Resources/Shaders/GBufferIdentity.vert".Load(), @"../../../Resources/Shaders/TerrainTileTextureCopy.frag".Load());
+        }
+
+        public void Render(Texture heightTexture, Texture paramTexture)
+        {
+            gb.Render(() =>
+            {
+                heightTexture.Bind(TextureUnit.Texture0);
+                paramTexture.Bind(TextureUnit.Texture1);
+            },
+            (sp) =>
+            {
+                sp.SetUniform("heightTex", 0);
+                sp.SetUniform("paramTex", 1);
+            });
+        }
+    }
+}
