afe5c4ed4d18ad9f87e6693ee01b5ccada527843
Geoff Thornburrow <geofftnz@gmail.com>
2014-05-09 21:53:45 +1200

    wip

---
+++ snowscape/Resources/Shaders/ParticleErosion.glsl
@@ -0,0 +1,67 @@
+﻿//|ComputeVelocity
+
+#version 140
+precision highp float;
+
+uniform sampler2D terraintex;
+uniform sampler2D particletex;
+uniform sampler2D velocitytex;
+uniform float texsize;
+uniform float vdecay;
+uniform float vadd;
+uniform float gravity;
+uniform float deltatime;
+uniform float speedCarryingCoefficient;
+
+in vec2 texcoord;
+
+out vec4 out_Velocity;
+
+float t = 1.0 / texsize;
+float diag = 0.707;
+
+
+float sampleHeight(vec2 pos)
+{
+	vec4 l = texture(terraintex,pos);
+	return l.r + l.g + l.b;
+}
+
+vec3 fallVector(vec2 p)
+{
+	float h0 = sampleHeight(p);
+	float h1 = sampleHeight(p + vec2(0,-t));
+	float h2 = sampleHeight(p + vec2(0,t));
+	float h3 = sampleHeight(p + vec2(-t,0));
+	float h4 = sampleHeight(p + vec2(t,0));
+
+	vec3 f = vec3(0);
+
+	f += vec3(0,-1,h1-h0) * (h0-h1);
+	f += vec3(0,1,h2-h0) * (h0-h2);
+	f += vec3(-1,0,h3-h0) * (h0-h3);
+	f += vec3(1,0,h4-h0) * (h0-h4);
+
+	return normalize(f);
+}
+
+void main(void)
+{
+	vec4 prevvel = texture(velocitytex,texcoord);
+	vec4 particle = texture(particletex,texcoord);
+
+	//  Uses slope information from L0 at position P0 to calculate acceleration of particle
+	vec3 fall = fallVector(particle.xy);
+	vec2 v = fall.xy;
+
+	//  Takes velocity from V1.rg, applies acceleration, writes to V0.rg
+	vec2 newVelocity = prevvel.xy * vdecay + v * vadd
+
+	//  Calculates new carrying capacity and writes to V0.b 
+	float speed = length(newVelocity);
+	float carryingCapacity = speed * speedCarryingCoefficient;
+	
+	out_Velocity = vec4(newVelocity.xy,carryingCapacity,0);
+}
+
+
--- snowscape/TerrainGeneration/GPUParticleErosion.cs
+++ snowscape/TerrainGeneration/GPUParticleErosion.cs
@@ -24,6 +24,7 @@ namespace TerrainGeneration
     /// 
     /// P0: particle state: RG = pos, B = carrying, A = ? (maybe death flag or carrying capacity)
     /// P1: particle state: RG = pos, B = carrying, A = ?
+    /// NOTE: pos is normalized 0-1
     /// 
     /// V0: particle velocity: RG = vel, B = new carrying capacity, A = death flag
     /// V1: particle velocity: RG = vel, B = new carrying capacity, A = death flag
@@ -107,7 +108,7 @@ namespace TerrainGeneration
             this.Width = width;
             this.Height = height;
             this.ParticleTexWidth = particleTexWidth;
-            this.ParticleTexHeight = particleTexHeight; 
+            this.ParticleTexHeight = particleTexHeight;
         }
 
 
@@ -154,7 +155,7 @@ namespace TerrainGeneration
 
         // Step 1: Determine particle velocities
         private GBufferShaderStep ComputeVelocityStep = new GBufferShaderStep("gpupe-1-velocity");
-        
+
         // Step 2: Accumulate erosion/sediment
         private VBO ParticleVertexVBO = new VBO("particle-vertex");
         private VBO ParticleIndexVBO = new VBO("particle-index", BufferTarget.ElementArrayBuffer);
@@ -166,7 +167,7 @@ namespace TerrainGeneration
 
         // Step 4: Update particle state
         private GBufferShaderStep UpdateParticlesStep = new GBufferShaderStep("gpupe-4-updateparticles");
-        
+
         // Step 5,6: Slip 
         private GBufferShaderStep SlippageFlowStep = new GBufferShaderStep("erosion-slipflow");
         private GBufferShaderStep SlippageTransportStep = new GBufferShaderStep("erosion-sliptransport");
@@ -218,54 +219,92 @@ namespace TerrainGeneration
 
             this.ErosionAccumulationTexture.UploadEmpty();
 
-
-            /// Step 1: Determine particle velocities
-            ///  - Render as quad over particles
-            ///  - Input: L0, P0, V1
-            ///  - Output: V0
-            ///  Uses slope information from L0 at position P0 to calculate acceleration of particle
-            ///  Takes velocity from V1.rg, applies acceleration, writes to V0.rg
-            ///  Calculates new carrying capacity and writes to V0.b 
-            ///  If slope information indicates particle is stuck, zero/reduce new carrying capacity, set death flag V0.a.
-            ///  
-            /// Step 2: Accumulate erosion/sediment
-            ///  - Render as particles over layer
-            ///  - Input: P0, V0
-            ///  - Output: E
-            ///   Vertex shader sets particle position in E based on P0.
-            ///   Calculate particle potential as new carrying capacity - old carrying capacity.
-            ///   Writes R:1 G:potential B:deposit
-            ///   Blend mode: add
-            ///  
-            /// Step 3: Update terrain layers
-            ///  - Render as quad over layer
-            ///  - Input: L0, E
-            ///  - Output: L1
-            ///  Adds deposit amount E.b to soft L0.g
-            ///  Subtracts material from soft, then hard.
-            ///  Modifies water depth from particle count (E.r).
-            ///  
-            /// Step 4: Update particle state
-            ///  - Render as quad over particles
-            ///  - Input: P0, V0, E, L0
-            ///  - Output: P1
-            ///  Replicate particle potential calc from Step 2 to get deposit/erode potentials.
-            ///  Subtract deposit amount from carrying amount P0.b, write to P1.b
-            ///  Apply same calculation as step 3 to determine how much soft/hard is being eroded from L0(P0.rg).
-            ///  Add material carried based on particle share of total V0.b / E(P0.rg).g -> P1.b
-            ///  Calculate new particle position by intersecting ray P0.rg->V0.rg against 
-            ///    cell boundaries. Add small offset to avoid boundary problems. Writes to P1.rg
-            ///  If death flag indicates particle recycle, init particle at random position.
-            ///  
-            /// Step 5: Slip flow
-            /// in: L1
-            /// out: flow
-            /// 
-            /// Step 6: Slip transport
-            /// in: L1,flow
-            /// out: L0    (avoids L0/L1 switch)
+            // init particle vbos
+            InitParticlesVBOs();
 
 
+
+            // Step 1: Determine particle velocities
+            //  - Render as quad over particles
+            //  - Input: L0, P0, V1
+            //  - Output: V0
+            //  Uses slope information from L0 at position P0 to calculate acceleration of particle
+            //  Takes velocity from V1.rg, applies acceleration, writes to V0.rg
+            //  Calculates new carrying capacity and writes to V0.b 
+            //  If slope information indicates particle is stuck, zero/reduce new carrying capacity, set death flag V0.a.
+            ComputeVelocityStep.SetOutputTexture(0, "out_Velocity", this.VelocityTexture[0]);
+            ComputeVelocityStep.Init(@"BasicQuad.vert", "@ParticleErosion.glsl|ComputeVelocity");
+
+
+            
+            // Step 2: Accumulate erosion/sediment
+            //  - Render as particles over layer
+            //  - Input: P0, V0, V1
+            //  - Output: E
+            //   Vertex shader sets particle position in E based on P0.
+            //   Calculate particle potential as new carrying capacity - old carrying capacity.
+            //   Writes R:1 G:potential B:deposit
+            //   Blend mode: add
+            //  
+            // Step 3: Update terrain layers
+            //  - Render as quad over layer
+            //  - Input: L0, E
+            //  - Output: L1
+            //  Adds deposit amount E.b to soft L0.g
+            //  Subtracts material from soft, then hard.
+            //  Modifies water depth from particle count (E.r).
+            //  
+            // Step 4: Update particle state
+            //  - Render as quad over particles
+            //  - Input: P0, V0, E, L0
+            //  - Output: P1
+            //  Replicate particle potential calc from Step 2 to get deposit/erode potentials.
+            //  Subtract deposit amount from carrying amount P0.b, write to P1.b
+            //  Apply same calculation as step 3 to determine how much soft/hard is being eroded from L0(P0.rg).
+            //  Add material carried based on particle share of total V0.b / E(P0.rg).g -> P1.b
+            //  Calculate new particle position by intersecting ray P0.rg->V0.rg against 
+            //    cell boundaries. Add small offset to avoid boundary problems. Writes to P1.rg
+            //  If death flag indicates particle recycle, init particle at random position.
+            //  
+            // Step 5: Slip flow
+            // in: L1
+            // out: flow
+            // 
+            // Step 6: Slip transport
+            // in: L1,flow
+            // out: L0    (avoids L0/L1 switch)
+
+
+        }
+
+
+        public void ModifyTerrain()
+        {
+            
+        }
+
+
+
+
+        private void InitParticlesVBOs()
+        {
+            Vector3[] vertex = new Vector3[this.ParticleTexWidth * this.ParticleTexHeight];
+
+            ParallelHelper.For2D(this.ParticleTexWidth, this.ParticleTexHeight, (x, y, i) =>
+            {
+                vertex[i] = new Vector3((float)x / (float)this.ParticleTexWidth, (float)y / (float)this.ParticleTexHeight, 0f);
+            });
+
+            this.ParticleVertexVBO.SetData(vertex);
+
+            uint[] index = new uint[this.ParticleTexWidth * this.ParticleTexHeight];
+
+            for (int i = 0; i < this.ParticleTexWidth * this.ParticleTexHeight; i++)
+            {
+                index[i] = (uint)i;
+            }
+
+            this.ParticleIndexVBO.SetData(index);
         }
 
         public void Unload()
@@ -273,29 +312,118 @@ namespace TerrainGeneration
             throw new NotImplementedException();
         }
 
-        public void ResetTerrain()
+      
+        private void UploadTerrain(float[] data)
         {
-            throw new NotImplementedException();
+            this.TerrainTexture[0].Upload(data);
         }
 
-        public void ModifyTerrain()
+        private float[] DownloadTerrain()
         {
-            throw new NotImplementedException();
-        }
-
-        public void Load(string filename)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Save(string filename)
-        {
-            throw new NotImplementedException();
+            return this.TerrainTexture[0].GetLevelDataFloat(0);
         }
 
         public float[] GetRawData()
         {
-            throw new NotImplementedException();
+            return DownloadTerrain();
         }
+
+
+        public void Load(string filename)
+        {
+            var data = new float[this.Width * this.Height * 4];
+
+            using (var fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.None, 256 * 1024))
+            {
+                using (var sr = new BinaryReader(fs))
+                {
+                    int magic = sr.ReadInt32();
+                    if (magic != FILEMAGIC)
+                    {
+                        throw new Exception("Not a terrain file");
+                    }
+
+                    int w, h;
+
+                    w = sr.ReadInt32();
+                    h = sr.ReadInt32();
+
+                    if (w != this.Width || h != this.Height)
+                    {
+                        // TODO: handle size changes
+                        throw new Exception(string.Format("Terrain size {0}x{1} did not match generator size {2}x{3}", w, h, this.Width, this.Height));
+                    }
+
+                    for (int i = 0; i < w * h; i++)
+                    {
+                        data[i * 4 + 0] = sr.ReadSingle();
+                        data[i * 4 + 1] = sr.ReadSingle();
+                        data[i * 4 + 2] = 0f; sr.ReadSingle();
+                        data[i * 4 + 3] = 0f; sr.ReadSingle();
+                    }
+
+                    sr.Close();
+                }
+                fs.Close();
+            }
+
+            UploadTerrain(data);
+
+        }
+
+        public void Save(string filename)
+        {
+            var data = this.DownloadTerrain();
+
+            using (var fs = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None, 256 * 1024))
+            {
+                using (var sw = new BinaryWriter(fs))
+                {
+                    sw.Write(FILEMAGIC);
+                    sw.Write(this.Width);
+                    sw.Write(this.Height);
+
+                    for (int i = 0; i < this.Width * this.Height; i++)
+                    {
+                        sw.Write(data[i * 4 + 0]);
+                        sw.Write(data[i * 4 + 1]);
+                        sw.Write(0f);
+                        sw.Write(0f);
+                    }
+
+                    sw.Close();
+                }
+                fs.Close();
+            }
+        }
+
+
+        public void ResetTerrain()
+        {
+            Snowscape.TerrainStorage.Terrain terrain = new Snowscape.TerrainStorage.Terrain(this.Width, this.Height);
+
+            terrain.Clear(0.0f);
+            terrain.AddSimplexNoise(4, 0.16f / (float)this.Width, 300.0f, h => h, h => Math.Abs(h));
+            terrain.AddSimplexNoise(14, 1.0f / (float)this.Width, 200.0f, h => Math.Abs(h), h => h);
+            //terrain.AddSimplexNoise(6, 19.0f / (float)this.Width, 20.0f, h => h*h, h => h);
+            terrain.AddLooseMaterial(1.0f);
+            terrain.SetBaseLevel();
+
+            var data = new float[this.Width * this.Height * 4];
+
+            ParallelHelper.For2D(this.Width, this.Height, (i) =>
+            {
+                data[i * 4 + 0] = terrain[i].Hard;
+                data[i * 4 + 1] = terrain[i].Loose;
+                data[i * 4 + 2] = 0.0f;  // water
+                data[i * 4 + 3] = 0.0f;
+            });
+
+            UploadTerrain(data);
+
+        }
+
+
+
     }
 }
