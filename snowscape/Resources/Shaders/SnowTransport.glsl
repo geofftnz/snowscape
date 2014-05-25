38c1ed7fc9913ebf676a86b7cf2ec5a9aaf3300a
Geoff Thornburrow <geofftnz@gmail.com>
2014-05-25 21:59:49 +1200

    start of snow transport

---
+++ snowscape/Resources/Shaders/SnowTransport.glsl
@@ -0,0 +1,25 @@
+﻿/*
+	GPU Snow transport model
+	(c)2014 Geoff Thornburrow
+
+	Parameters:
+	- Wind vector
+	- Snowfall rate
+	- Temperature (? maybe later for melt/freeze)
+
+	Terrain:
+	R: hard
+	G: soft
+	B: packed snow
+	A: powder
+
+
+
+	Steps:
+
+	1: Snowfall
+	2: Powder slip
+	3: Powder compaction
+
+	
+*/
