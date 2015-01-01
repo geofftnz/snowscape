2c2647bd988cbbe283604d1c59d9ac1002337e34
Geoff Thornburrow <geofftnz@gmail.com>
2015-01-01 21:13:27 +1300

    adding file excluded by gitignore

---
+++ snowscape/TerrainGenerationViewer/UI/DebugUI/QuadTreeLodDebugRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTKExtensions.Components;
+using OpenTKExtensions.Framework;
+
+namespace Snowscape.TerrainGenerationViewer.UI.Debug
+{
+    public class QuadTreeLodDebugRenderer : GameComponentBase, IRenderable
+    {
+        private GameComponentCollection Components = new GameComponentCollection();
+        private LineBuffer lineBuffer;
+
+        public bool Visible { get; set; }
+        public int DrawOrder { get; set; }
+
+        public QuadTreeLodDebugRenderer()
+        {
+            this.Visible = true;
+            this.DrawOrder = 0;
+
+            this.Components.Add(lineBuffer = new LineBuffer(8192));
+            this.Loading += QuadTreeLodDebugRenderer_Loading;
+
+        }
+
+        void QuadTreeLodDebugRenderer_Loading(object sender, EventArgs e)
+        {
+            this.Components.Load();
+
+        }
+
+
+
+        public void Render(IFrameRenderData frameData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
