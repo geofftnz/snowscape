b82456d97bd1a360058a5f72737408d9cd0a6059
Geoff Thornburrow <geofftnz@gmail.com>
2015-03-28 22:23:19 +1300

    starting conical mesh renderer

---
+++ snowscape/OpenTKExtensions/Generators/GridMesh.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace OpenTKExtensions.Generators
+{
+    /// <summary>
+    /// Generates streams of vertices and indexes that can be loaded into a VBO
+    /// width and height are cell count
+    /// </summary>
+    public class GridMesh
+    {
+        private int width;
+        private int height;
+        private int vwidth { get { return width + 1; } }
+        private int vheight { get { return height + 1; } }
+
+        public GridMesh(int width, int height)
+        {
+            if (width < 1 || height < 1)
+                throw new InvalidOperationException("Width & height must be at least 2");
+
+            this.width = width;
+            this.height = height;
+        }
+
+        public IEnumerable<Vector3> Vertices(Action<Vector3, float> axis1, Action<Vector3, float> axis2)
+        {
+            for (int y = 0; y < vheight; y++)
+            {
+                float yc = (float)y / (float)(height);
+
+                for (int x = 0; x < vwidth; x++)
+                {
+                    float xc = (float)x / (float)width;
+
+                    Vector3 v = Vector3.Zero;
+
+                    axis1(v, xc);
+                    axis2(v, yc);
+
+                    yield return v;
+                }
+
+            }
+        }
+
+        public IEnumerable<Vector3> VerticesXZ()
+        {
+            return Vertices((v, a) => { v.X = a; }, (v, b) => { v.Z = b; });
+        }
+
+        public IEnumerable<uint> Indices()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    // 0 1
+                    // 2 3
+
+                    // 0 2 1 
+                    // 1 2 3 
+                    yield return (uint)(x + y * vwidth); // 0 
+                    yield return (uint)(x + (y + 1) * vwidth); // 2
+                    yield return (uint)((x + 1) + y * vwidth); // 1
+                    yield return (uint)((x + 1) + y * vwidth); // 1
+                    yield return (uint)(x + (y + 1) * vwidth); // 2
+                    yield return (uint)((x + 1) + (y + 1) * vwidth); // 3
+
+                }
+            }
+        }
+
+    }
+}
---
+++ snowscape/TerrainRenderer/Mesh/ConicalMesh.cs
@@ -0,0 +1,61 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTKExtensions;
+using OpenTKExtensions.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Snowscape.TerrainRenderer.Mesh
+{
+
+    /// <summary>
+    /// experimental mesh with a constant LoD
+    /// 
+    /// Regular square mesh, but transformed in vertex shader.
+    /// </summary>
+    public class ConicalMesh : GameComponentBase, IGameComponent
+    {
+        private VBO vertexVBO = new VBO("patch-vertex");
+        private VBO indexVBO = new VBO("patch-index", BufferTarget.ElementArrayBuffer);
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public ConicalMesh(int width, int height)
+        {
+            if (width < 1 || height < 1)
+            {
+                throw new InvalidOperationException("Conical: Width and height must be at least 1");
+            }
+            this.Width = width;
+            this.Height = height;
+
+            this.Loading += ConicalMesh_Loading;
+        }
+
+        void ConicalMesh_Loading(object sender, EventArgs e)
+        {
+            var meshgenerator = new OpenTKExtensions.Generators.GridMesh(Width, Height);
+
+            this.vertexVBO.SetData(meshgenerator.VerticesXZ().ToArray());
+            this.indexVBO.SetData(meshgenerator.Indices().ToArray());
+
+        }
+
+        public void Bind(int vertexLocation)
+        {
+            this.vertexVBO.Bind(vertexLocation);
+            this.indexVBO.Bind();
+        }
+
+        public void Render()
+        {
+            GL.DrawElements(BeginMode.Triangles, this.indexVBO.Length, DrawElementsType.UnsignedInt, 0);
+        }
+
+
+
+    }
+}
