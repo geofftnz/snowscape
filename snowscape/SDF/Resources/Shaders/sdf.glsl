8ab3923774c69e298089f8d556dc6fc2c8e7f1c6
Geoff Thornburrow <geofftnz@gmail.com>
2015-04-28 22:30:11 +1200

    replaced comment in shader

--- snowscape/SDF/Resources/Shaders/sdf.glsl
+++ snowscape/SDF/Resources/Shaders/sdf.glsl
@@ -35,7 +35,31 @@ out vec4 out_Col;
 
 
 
+/*
+https://www.shadertoy.com/view/4sfGDB
 
+This shader is an attempt at porting smallpt to GLSL.
+
+See what it's all about here:
+http://www.kevinbeason.com/smallpt/
+
+The code is based in particular on the slides by David Cline.
+
+Some differences:
+
+- For optimization purposes, the code considers there is
+  only one light source (see the commented loop)
+- Russian roulette and tent filter are not implemented
+
+I spent quite some time pulling my hair over inconsistent
+behavior between Chrome and Firefox, Angle and native. I
+expect many GLSL related bugs to be lurking, on top of
+implementation errors. Please Let me know if you find any.
+
+--
+Zavie
+
+*/
 
 
 
