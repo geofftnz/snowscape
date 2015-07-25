d150d36c6afde8999ed8d3c3a681e51c5992a807
Geoff Thornburrow <geofftnz@gmail.com>
2015-07-26 11:57:19 +1200

    detail generator start

---
+++ snowscape/TerrainRenderer/TerrainDetail/DetailGenerator.cs
@@ -0,0 +1,21 @@
+﻿using OpenTKExtensions;
+using OpenTKExtensions.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snowscape.TerrainRenderer.TerrainDetail
+{
+
+    /// <summary>
+    /// Generates a higher level of detail from the base terrain maps.
+    /// </summary>
+    public class DetailGenerator : GameComponentBase
+    {
+        public Texture HeightTexture { get; set; }
+
+
+
+    }
+}
