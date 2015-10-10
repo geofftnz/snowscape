697e9b7716bb6b46b382ff00ce89408a4fd4a717
Geoff Thornburrow <geofftnz@gmail.com>
2015-10-10 19:44:41 +1300

    detailgenerator

---
+++ snowscape/Resources/Shaders/DetailGenerator.glsl
@@ -0,0 +1,48 @@
+﻿//|vert
+#version 140
+precision highp float;
+in vec3 vertex;
+out vec2 texcoord;
+
+void main() {
+
+	gl_Position = vec4(vertex.xy,0.0,1.0);
+	texcoord = vertex.xy * 0.5 + 0.5;
+}
+
+//|frag
+#version 140
+precision highp float;
+
+uniform sampler2D heighttex;
+uniform sampler2D paramtex;
+uniform float invtexsize;
+
+in vec2 texcoord;
+
+out float out_Height;
+out vec4 out_Normal;
+out vec4 out_Param;
+
+float t = invtexsize;
+
+float sampleHeight(vec2 pos)
+{
+	return textureLod(heighttex,pos,0).r;
+}
+
+vec3 getNormal(vec2 pos)
+{
+    float h1 = sampleHeight(vec2(pos.x, pos.y - t));
+	float h2 = sampleHeight(vec2(pos.x, pos.y + t));
+    float h3 = sampleHeight(vec2(pos.x - t, pos.y));
+	float h4 = sampleHeight(vec2(pos.x + t, pos.y));
+	return normalize(vec3(h3-h4,2.0,h1-h2));
+}
+
+void main(void)
+{
+	out_Height = sampleHeight(texcoord);
+	out_Normal = vec4(getNormal(texcoord),1.0);
+	out_Param = textureLod(paramtex,texcoord,0);
+}
--- snowscape/TerrainRenderer/TerrainDetail/DetailGenerator.cs
+++ snowscape/TerrainRenderer/TerrainDetail/DetailGenerator.cs
@@ -1,4 +1,6 @@
-﻿using OpenTKExtensions;
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTKExtensions;
 using OpenTKExtensions.Framework;
 using System;
 using System.Collections.Generic;
@@ -11,14 +13,107 @@ namespace Snowscape.TerrainRenderer.TerrainDetail
     /// <summary>
     /// Generates a higher level of detail from the base terrain maps.
     /// 
-    /// 
+    /// On render
     /// </summary>
-    public class DetailGenerator : GameComponentBase
+    public class DetailGenerator : GameComponentBase, IListTextures
     {
-
+        // output textures 
         public Texture HeightTexture { get; private set; }
+        public Texture NormalTexture { get; private set; }
+        public Texture ParamTexture { get; private set; }
 
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        // shader step for detail generation
+        private GBufferShaderStep gb = new GBufferShaderStep("detailgen");
+
+        /// <summary>
+        /// Current position of center in world coordinates. 
+        /// (This will get multiplied up by scale.)
+        /// </summary>
+        public Vector2 Position { get; set; }
+
+        /// <summary>
+        /// last generated position
+        /// </summary>
+        //private Vector2 LastPosition = new Vector2(-1f, -1f);
+        
+
+        /// <summary>
+        /// Scale factor of detail generation (likely to be 16)
+        /// </summary>
+        public float Scale { get; set; }
+
+        public DetailGenerator(int width, int height)
+            : base()
+        {
+            this.Width = width;
+            this.Height = height;
+
+            this.Loading += DetailGenerator_Loading;
+            this.Unloading += DetailGenerator_Unloading;
+        }
+
+        void DetailGenerator_Unloading(object sender, EventArgs e)
+        {
+            this.HeightTexture.Unload();
+            this.NormalTexture.Unload();
+            this.ParamTexture.Unload();
+        }
+
+        void DetailGenerator_Loading(object sender, EventArgs e)
+        {
+            this.HeightTexture =
+                new Texture("Height", this.Width, this.Height, TextureTarget.Texture2D, PixelInternalFormat.R32f, PixelFormat.Red, PixelType.Float)
+                .SetParameter(new TextureParameterInt(TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear))
+                .SetParameter(new TextureParameterInt(TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear))
+                .SetParameter(new TextureParameterInt(TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat))
+                .SetParameter(new TextureParameterInt(TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat));
+
+            this.NormalTexture = new Texture("Normal", this.Width, this.Height, TextureTarget.Texture2D, PixelInternalFormat.Rgba16f, PixelFormat.Rgba, PixelType.HalfFloat)
+                .SetParameter(new TextureParameterInt(TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear))
+                .SetParameter(new TextureParameterInt(TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear))
+                .SetParameter(new TextureParameterInt(TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat))
+                .SetParameter(new TextureParameterInt(TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat));
+
+            this.ParamTexture = new Texture("Param", this.Width, this.Height, TextureTarget.Texture2D, PixelInternalFormat.Rgba, PixelFormat.Rgba, PixelType.UnsignedByte)
+                .SetParameter(new TextureParameterInt(TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear))
+                .SetParameter(new TextureParameterInt(TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear))
+                .SetParameter(new TextureParameterInt(TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat))
+                .SetParameter(new TextureParameterInt(TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat));
+
+            
+            gb.SetOutputTexture(0, "out_Height", this.HeightTexture);
+            gb.SetOutputTexture(1, "out_Normal", this.NormalTexture);
+            gb.SetOutputTexture(2, "out_Param", this.ParamTexture);
+            gb.Init(@"DetailGenerator.glsl|vert", @"DetailGenerator.glsl|frag");
+        }
+
+        public IEnumerable<Texture> Textures()
+        {
+            yield return HeightTexture;
+            yield return NormalTexture;
+            yield return ParamTexture;
+        }
+
+        public void Render(Texture inputHeightTexture, Texture inputParamTexture)
+        {
+            gb.Render(() =>
+            {
+                inputHeightTexture.Bind(TextureUnit.Texture0);
+                inputParamTexture.Bind(TextureUnit.Texture1);
+            },
+            (sp) =>
+            {
+                sp.SetUniform("heighttex", 0);
+                sp.SetUniform("paramtex", 1);
+                sp.SetUniform("invtexsize", 1.0f/(float)inputHeightTexture.Width);
+            });
+
+        }
 
 
     }
 }
+
